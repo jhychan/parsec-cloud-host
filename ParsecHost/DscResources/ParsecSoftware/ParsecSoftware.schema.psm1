8bc57b97d0c6d651e3e85bdff0b67cba335bd5ba
j <justcme@gmail.com>
2020-04-06 18:26:37 +0100

    Set dependencies in software installs; explicity set chocolateyinstall env var to workaround issues with chocolatey dsc resource

--- ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
+++ ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
@@ -2,11 +2,23 @@ Configuration ParsecSoftware
 {
     Param()
 
+    Import-DscResource -ModuleName 'PSDscResources'
     Import-DscResource -ModuleName 'chocolatey'
 
+    Environment 'ChocolateyInstallPath'
+    {
+        Ensure = 'Present'
+        Name = 'ChocolateyInstall'
+        Value = Join-Path $env:ProgramData 'chocolatey'
+        Path = $false
+        Target = 'Process','Machine'
+    }
+    
     ChocolateySoftware 'Chocolatey'
     {
         Ensure = 'Present'
+        InstallationDirectory = $env:ChocolateyInstall
+        DependsOn = '[Environment]ChocolateyInstallPath'
     }
 
     ChocolateyPackage '7zip'
@@ -14,6 +26,7 @@ Configuration ParsecSoftware
         Ensure = 'Present'
         Name = '7zip'
         Version = 'Latest'
+        DependsOn = '[ChocolateySoftware]Chocolatey'
     }
 
     ChocolateyPackage 'Chrome'
@@ -21,5 +34,6 @@ Configuration ParsecSoftware
         Ensure = 'Present'
         Name = 'googlechrome'
         Version = 'Latest'
+        DependsOn = '[ChocolateySoftware]Chocolatey'
     }
 }
