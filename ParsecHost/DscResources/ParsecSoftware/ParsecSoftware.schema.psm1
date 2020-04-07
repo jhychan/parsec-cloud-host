be72fcf0d168cc35b9fe517dc6d12209763a37ed
j <justcme@gmail.com>
2020-04-07 18:09:28 +0100

    Add parsec install configuration; generates chocolatey package and applies chocolatey package resource

--- ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
+++ ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
@@ -5,19 +5,20 @@ Configuration ParsecSoftware
     Import-DscResource -ModuleName 'PSDscResources'
     Import-DscResource -ModuleName 'chocolatey'
 
+    $chocolateyInstallPath = Join-Path $env:ProgramData 'chocolatey'
     Environment 'ChocolateyInstallPath'
     {
         Ensure = 'Present'
         Name = 'ChocolateyInstall'
-        Value = Join-Path $env:ProgramData 'chocolatey'
+        Value = $chocolateyInstallPath
         Path = $false
         Target = 'Process','Machine'
     }
-    
+
     ChocolateySoftware 'Chocolatey'
     {
         Ensure = 'Present'
-        InstallationDirectory = $env:ChocolateyInstall
+        InstallationDirectory = $chocolateyInstallPath
         DependsOn = '[Environment]ChocolateyInstallPath'
     }
 
@@ -35,5 +36,52 @@ Configuration ParsecSoftware
         Name = 'googlechrome'
         Version = 'Latest'
         DependsOn = '[ChocolateySoftware]Chocolatey'
+    }
+
+    # Generated parsec chocolatey package
+    $packageName = 'parsecgaming'
+    $packageVersion = '1.0'
+    $packageSourceFolder = Join-Path $chocolateyInstallPath 'local'
+    $packageFolder = Join-Path $packageSourceFolder $packageName
+    File 'ParsecInstallerPackageSource'
+    {
+        Ensure = 'Present'
+        DestinationPath = $packageFolder
+        Type = 'Directory'
+        DependsOn = '[ChocolateySoftware]Chocolatey'
+    }
+    $packageFile = "$packageName.$packageVersion.nupkg"
+    $packageFilePath = Join-Path $packageFolder $packageFile
+    $packageNuspec = Join-Path $PSScriptRoot "..\..\Packages\$packageName\$packageName.nuspec"
+    Script 'ParsecInstallerPackage'
+    {
+        SetScript = { choco.exe pack $using:packageNuspec --outputdirectory $using:packageFolder }
+        TestScript = { Test-Path -Path $using:packageFilePath }
+        GetScript = { @{ Result = Get-Item $using:packageFilePath } }
+        DependsOn = '[ChocolateySoftware]Chocolatey','[File]ParsecInstallerPackageSource'
+    }
+
+    # Install parsec using the generated package
+    ChocolateyPackage 'Parsec'
+    {
+        Ensure = 'Present'
+        Name = 'parsecgaming'
+        Version ='Latest'
+        ChocolateyOptions = @{
+            'source' = $packageSourceFolder
+        }
+        DependsOn = '[Script]ParsecInstallerPackage'
+    }
+
+    # Configure parsec autorun for all users
+    $parsecFilePath = Join-Path $env:ProgramFiles 'Parsec\parsecd.exe'
+    Registry 'ParsecAutorun'
+    {
+        Ensure = 'Present'
+        Key = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Run'
+        ValueName = 'Parsec.App.0'
+        ValueData = "$parsecFilePath app_silent=1"
+        ValueType = 'String'
+        DependsOn = '[ChocolateyPackage]Parsec'
     }
 }
