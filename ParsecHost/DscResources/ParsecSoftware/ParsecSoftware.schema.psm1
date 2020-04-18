322c5fa8559b0a0b51f9a001e77cc4e925faff30
j <justcme@gmail.com>
2020-04-18 16:15:59 +0100

    Create shared config in ProgramData and symlink before installing Parsec

--- ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
+++ ParsecHost/DscResources/ParsecSoftware/ParsecSoftware.schema.psm1
@@ -52,6 +52,56 @@ Configuration ParsecSoftware
         DependsOn = '[ChocolateySoftware]Chocolatey'
     }
 
+    # Set up shared parsec config directory
+    $currentConfigDir = Join-Path $env:ProgramData 'Parsec'
+    $currentConfigPath = Join-Path $currentConfigDir 'config.txt'
+    File 'ParsecConfigFile'
+    {
+        Ensure = 'Present'
+        DestinationPath = $currentConfigPath
+        Type = 'File'
+        Contents = @"
+# All configuration settings must appear on a new line.
+# All whitespace, besides the newline character '\n', is ignored.
+# All settings passed via the command line take precedence.
+# The configuration file will be overwritten by Parsec when changing settings,
+#   so if you edit this file while Parsec is running, make sure to save this file
+#   and restart Parsec immediately so your changes are preserved.
+
+# Example:
+# encoder_bitrate = 10
+app_run_level = 3
+
+encoder_bitrate=50
+encoder_min_bitrate = 20
+#encoder_vbv_max = 500
+#encoder_min_qp=5
+
+server_audio_cancel=0
+network_server_start_port = 8000
+"@
+    }
+
+    # Symlink the config directory for all enabled users
+    $users = Get-LocalUser | ? { $_.Enabled }
+    ForEach($username in $users.Name) {
+        $parsecConfigDir = Join-Path $env:SystemDrive "Users\$username\AppData\Roaming\Parsec"
+        Script 'ParsecUserConfigFolder'
+        {
+            TestScript = {
+                # if folder exists then do nothing - either already hardlinked, or current session is the autologon account
+                Test-Path $using:parsecConfigDir
+            }
+            GetScript = {
+                @{ Result = Get-Item $using:parsecConfigDir }
+            }
+            SetScript = {
+                New-Item -ItemType 'Junction' -Path $using:parsecConfigDir -Value $using:currentConfigDir
+            }
+            Dependson = '[File]ParsecConfigFile'
+        }
+    }
+
     # Generated parsec chocolatey package
     $packageName = 'parsecgaming'
     $packageVersion = '1.0'
@@ -91,38 +141,7 @@ Configuration ParsecSoftware
         ChocolateyOptions = @{
             'source' = $packageSourceFolder
         }
-        DependsOn = '[Script]ParsecInstallerPackage'
-    }
-
-    # Set some defaults
-    $currentConfigDir = Join-Path $env:AppData 'Parsec'
-    $currentConfigPath = Join-Path $currentConfigDir 'config.txt'
-    File 'ParsecConfigFile'
-    {
-        Ensure = 'Present'
-        DestinationPath = $currentConfigPath
-        Type = 'File'
-        Contents = @"
-# All configuration settings must appear on a new line.
-# All whitespace, besides the newline character '\n', is ignored.
-# All settings passed via the command line take precedence.
-# The configuration file will be overwritten by Parsec when changing settings,
-#   so if you edit this file while Parsec is running, make sure to save this file
-#   and restart Parsec immediately so your changes are preserved.
-
-# Example:
-# encoder_bitrate = 10
-app_run_level = 3
-
-encoder_bitrate=50
-encoder_min_bitrate = 20
-#encoder_vbv_max = 500
-#encoder_min_qp=5
-
-server_audio_cancel=0
-network_server_start_port = 8000
-"@
-        DependsOn = '[ChocolateyPackage]Parsec'
+        DependsOn = '[Script]ParsecInstallerPackage','[Script]ParsecUserConfigFolder'
     }
 
     # Configure parsec autolaunch via scheduled task for the logged in user
@@ -140,7 +159,7 @@ network_server_start_port = 8000
         ScheduleType = 'AtLogOn'
         ActionExecutable = $parsecFilePath
         MultipleInstances = 'IgnoreNew'
-        DependsOn = '[File]ParsecConfigFile'
+        DependsOn = '[ChocolateyPackage]Parsec'
     }
 
     # Use scheduled task to launch parsec in current session for setting up login
@@ -159,20 +178,5 @@ network_server_start_port = 8000
         DependsOn = '[ScheduledTask]ParsecAutorun'
     }
 
-    # Current user might not be the autologon user - hardlink the parsec config dir if not
-    $parsecConfigDir = Join-Path $env:SystemDrive "Users\$($Credential.UserName)\AppData\Roaming\Parsec"
-    Script 'ParsecUserConfigFolder'
-    {
-        TestScript = {
-            # if folder exists then do nothing - either already hardlinked, or current session is the autologon account
-            Test-Path $using:parsecConfigDir
-        }
-        GetScript = {
-            @{ Result = Get-Item $using:parsecConfigDir }
-        }
-        SetScript = {
-            New-Item -ItemType 'Junction' -Path $using:parsecConfigDir -Value $using:currentConfigDir
-        }
-        Dependson = '[Script]ParsecRunning'
-    }
+    
 }
